89b2e16d3e59e480d744b0070f6d846b342eaefa
K Vijith <vijith.messi@gmail.com>
2021-10-07 11:00:37 +0530

    Add project files.

---
+++ SharpBank.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpBank.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
