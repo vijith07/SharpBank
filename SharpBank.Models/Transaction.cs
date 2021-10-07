39971cef63bd16e134453a532c4e59fcc2cff5bb
Shriram <18211a04l2@bvrit.ac.in>
2021-10-07 21:44:14 +0530

    Added Transaction Services

--- SharpBank.Models/Transaction.cs
+++ SharpBank.Models/Transaction.cs
@@ -9,18 +9,18 @@ namespace SharpBank.Models
     public class Transaction
     {
         public string TransactionID { get; set; }
-        public string RecepientBank { get; set; }
-        public string SenderBank { get; set; }
+        public string RecepientIFSC { get; set; }
+        public string SenderIFSC { get; set; }
         public string RecepientAccount { get; set; }
         public string SenderAccount { get; set; }
         public decimal Amount { get; set; }
          
         
-        public Transaction(string transactionID, string recepientBank, string senderBank, string recepientAccount, string senderAccount, decimal amount)
+        public Transaction(string transactionID, string senderIFSC, string senderAccount,  string recepientIFSC, string recepientAccount, decimal amount)
         {
             TransactionID = transactionID;
-            RecepientBank = recepientBank;
-            SenderBank = senderBank;
+            RecepientIFSC = recepientIFSC;
+            SenderIFSC = senderIFSC;
             RecepientAccount = recepientAccount;
             SenderAccount = senderAccount;
             Amount = amount;
---
+++ SharpBank.Services/TransactionServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpBank.Models;
+using SharpBank.Models.Exceptions;
+
+namespace SharpBank.Services
+{
+    class TransactionServices
+    {
+        private string generateTransactionID() 
+        {
+            int serialNumber = BankManager.Transactions.Count + 1;
+            return serialNumber.ToString();
+        }
+        public void AddTransaction(string senderIFSC, string sender, string receiverIFSC, string receiver,decimal amount) 
+        {
+            string id = generateTransactionID();
+            Transaction txn = new Transaction(id, senderIFSC, sender, receiverIFSC, receiver, amount);
+            BankManager.Transactions.Add(id,txn);
+        }
+    }
+}
