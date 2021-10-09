492d9e57582a85830f0d4fab58780ae8076afed0
K Vijith <vijith.messi@gmail.com>
2021-10-09 12:42:41 +0530

    Refactored Models Using DDD

--- SharpBank.Models/Transaction.cs
+++ SharpBank.Models/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,31 +9,32 @@ namespace SharpBank.Models
 {
     public class Transaction
     {
+        [Key]
         public string TransactionID { get; set; }
+        [Required]
         public string RecepientIFSC { get; set; }
+        [Required]
         public string SenderIFSC { get; set; }
+        [Required]
         public string RecepientAccount { get; set; }
+        [Required]
         public string SenderAccount { get; set; }
+        [Required]
         public decimal Amount { get; set; }
 
-        public override string ToString() {
-            string res = "";
-            res+="Transaction ID : " + this.TransactionID + Environment.NewLine;
-            res+="-------------------"+Environment.NewLine;
-            res+="Recepient IFSC : " + this.RecepientAccount+ Environment.NewLine + "Recepient Acc Number : " + this.RecepientAccount +Environment.NewLine;
-            res += "Sender IFSC : " + this.SenderAccount + Environment.NewLine+"Sender Acc Number : " + this.SenderAccount + Environment.NewLine;
-            res += "Transacted Amount : " + this.Amount +"bucks"+ Environment.NewLine;
-            return res;
-        }
+        //public override string ToString() {
+        //    string res = "";
+        //    res+="Transaction ID : " + this.TransactionID + Environment.NewLine;
+        //    res+="-------------------"+Environment.NewLine;
+        //    res+="Recepient IFSC : " + this.RecepientAccount+ Environment.NewLine + "Recepient Acc Number : " + this.RecepientAccount +Environment.NewLine;
+        //    res += "Sender IFSC : " + this.SenderAccount + Environment.NewLine+"Sender Acc Number : " + this.SenderAccount + Environment.NewLine;
+        //    res += "Transacted Amount : " + this.Amount +"bucks"+ Environment.NewLine;
+        //    return res;
+        //}
 
-        public Transaction(string transactionID, string senderIFSC, string senderAccount,  string recepientIFSC, string recepientAccount, decimal amount)
+        public Transaction()
         {
-            TransactionID = transactionID;
-            RecepientIFSC = recepientIFSC;
-            SenderIFSC = senderIFSC;
-            RecepientAccount = recepientAccount;
-            SenderAccount = senderAccount;
-            Amount = amount;
+           
         }
     }
 }
